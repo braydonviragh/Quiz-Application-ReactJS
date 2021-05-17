caff530003471c2d6c8f4e32d8341f4093f8ff29
Braydon Viragh <bradyviragh@gmail.com>
2021-05-17 09:34:50 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,44 +11,44 @@ function Home() {
             <div className="flex justify-center flex-col w-full justify-center items-start py-10 p-10 ">
                 <h1 className="font-bold text-3xl my-4">YOU THINK YOU CAN QUIZ?</h1>
                 <p className="leading-normal mb-4">Well, do you ? Below are several skill testing quizzes supplied by the Open Trivia Database API. Each quiz comes with 10 multiple choice questions in each listed category. <br/> HOW TO PLAY: Choose topic below, read question and click on 1 of 4 answers provided. </p>
-                <div className="container md:text-lg mt-5 flex flex-wrap justify-around ">
+                <div className="container md:text-lg mt-5 grid grid-cols-2 gap-6 ">
                 <NavLink exact to="/Quiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2  px-6 border border-blue-500 hover:border-transparent rounded">
                         Anime
                         </button>
                     </NavLink>
                     <NavLink exact to="/AnimalsQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 x-6 border border-blue-500 hover:border-transparent rounded">
                         Animals
                         </button>
                     </NavLink>
                     <NavLink exact to="/GeneralQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded">
                         General
                         </button>
                     </NavLink>
                     <NavLink exact to="/HistoryQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded">
                         History
                         </button>
                     </NavLink>
                     <NavLink exact to="/MoviesQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded">
                         Movies
                         </button>
                     </NavLink>
                     <NavLink exact to="/MusicalQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded">
                         Music
                         </button>
                     </NavLink>
                     <NavLink exact to="/SportsQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 px-6 border border-blue-500 hover:border-transparent rounded">
                         Sports
                         </button>
                     </NavLink>
                     <NavLink exact to="/TVQuiz">
-                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2 m-2 px-6 border border-blue-500 hover:border-transparent rounded">
+                        <button className="bg-white hover:bg-blue-500 text-blue-700 w-full font-semibold hover:text-white py-2  px-6 border border-blue-500 hover:border-transparent rounded">
                         Television
                         </button>
                     </NavLink>
